bf80218f137651c1b1815d6fdb68d94d0e5d039a
alejandro.laneri@students.ffhs.ch <alejandro.laneri@students.ffhs.ch>
2021-07-28 19:01:06 +0200

    Configuration done. @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) in File StockRestApiApplication.java was addes, because it throws the following Error while building ***************************                                                                                                      APPLICATION FAILED TO START                                                                                                      ***************************
                                                                                                         Description:
    
                                                                                                         Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured.

--- src/main/java/ch/personal/stock/StockRestApiApplication.java
+++ src/main/java/ch/personal/stock/StockRestApiApplication.java
@@ -1,13 +1,17 @@
 package ch.personal.stock;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class StockRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockRestApiApplication.class, args);
+
     }
 
 }
