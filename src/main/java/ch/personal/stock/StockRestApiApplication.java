02074c133a3c86fafe9a47d55e88c5aa02c626b4
alejandro.laneri@students.ffhs.ch <alejandro.laneri@students.ffhs.ch>
2021-07-28 18:40:29 +0200

    FirstLady

---
+++ src/main/java/ch/personal/stock/StockRestApiApplication.java
@@ -0,0 +1,13 @@
+package ch.personal.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/personal/stock/StockRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.personal.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
